d4abe6c49442ead30dd6e71b6173e3428541d017
Eric <eric.valbot@educa.jcyl.es>
2025-03-06 15:40:34 +0100

    registro de usuarios

--- view/form_registro.php
+++ view/form_registro.php
@@ -4,31 +4,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./estilos/index.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="../public/css/login.css">
+    <script src="../public/mostrarContrasena.js"></script>
+    <script src="../public/validacionRegistro.js"></script>
     <title>Registro</title>
 </head>
 
 <body>
-    <div class="titulo">
-        <h1>Sistema de Gestión de tareas</h1>
-        <p>Este sistema te permite crear tareas y modificarlas</p>
-    </div>
-    <div>
-        <h1></h1>
-        <form action="../control/registro.php" method="post">
-            <input type="text" id="username" name="username" placeholder="Username" required>
-            <br>
-            <input type="email" id="email" name="email" placeholder="Email" required>
-            <br>
-            <input type="password" id="password" name="password" placeholder="Password" required>
-            <button type="submit" name="enviar" id="enviar">Registrase</button>
-        </form>
-        <div class="linea-container">
-            <div class="linea"></div>
-            <span class="o">o</span>
-            <div class="linea"></div>
+    <main>
+        <div class="titulo">
+            <h1>Registrate</h1>
+            <h3 id="frase">Para poder usar el sistema es necesaria una cuenta</h3>
         </div>
-    </div>
+        <div class="fuera">
+            <h2>Login</h2>
+            <form action="../control/registro.php" method="post" id="formulario">
+                <div class="input-container">
+                    <input type="text" id="username" name="username" placeholder="Username" required>
+                    <p id="nombreError"></p>
+                </div>
+                <input type="email" id="email" name="email" placeholder="Email" required>
+                <p id="emailError"></p>
+                <br>
+                <div class="input-container divContrasena">
+                    <input type="password" id="password" name="password" placeholder="Password" required>
+                    <i class="bx bx-show-alt" name="mostrarContrasena" id="mostrarContrasena"></i>
+                    <p id="passwdError"></p>
+                </div>
+                <button type="submit" name="enviar" id="enviar">Registrase</button>
+            </form>
+        </div>
+    </main>
 </body>
 
 </html>
